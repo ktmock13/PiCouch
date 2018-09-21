39809eabcc43796b7ef9cff68a7150d1209459c7
Keith Mock <kmock@apple.com>
2018-09-21 15:34:04 -0700

    fix up tests

--- accessories/Couch_test.js
+++ accessories/Couch_test.js
@@ -7,35 +7,42 @@ var COUCH = {
     setPosition: function (value) {
 
         // if postition is 100, or 0 go a bit longer to ensure closed/open fully
-        console.log("Setting couch position to %s percent", value);
+        // console.log("Setting couch position to %s percent", value);
         const moveDifference = COUCH.position - value;
         const directionOpen = moveDifference < 0; // ex: asked for 70% while the couch is @ 60%, -10 means move open more.
 
         // round absolute value of percent, then multiply by full motion time to get seconds to move
-        let duration = COUCH.secondsToComplete * (Math.abs(moveDifference) / 100)
+        let duration = Math.round(COUCH.secondsToComplete * (Math.abs(moveDifference) / 100) * 100) / 100;
 
-        // account for slop in the timing
+        // TWEAKS FOR SLOP IN THE TIMING
 
-        // since the couch opens slower
+        // Since the couch opens slower with your fat ass in it
         if (directionOpen) {
             duration += COUCH.openingOffset * (duration / COUCH.secondsToComplete);
         }
 
-        duration = Math.round(duration * 100) / 100;
-
-        // to be sure it goes all the way, add 1 sec
+        // To be sure it goes all the way open/closed, add 1 sec. The couch's killswitch won't let the motor strain.
         if (!value || value === 100) duration++
 
         // trigger move_couch.py with duration and direction
-        cmd.get('python ../python/couch_move_test.py ' + duration + ' ' + directionOpen, function (err, str, stderr) {
-            console.log(str);
-        });
+        try {
+            // console.log('attempting cmd ' + 'sudo python ./python/couch_move_test.py ' + duration + ' ' + directionOpen);
+            cmd.get('sudo python ./python/left_couch_move.py ' + duration + ' ' + directionOpen, function (err, str, stderr) {
+                if (err) console.error(err);
+            });
+        } catch (err) {
+            console.error(err);
+        }
+
         // set new couch position
         COUCH.position = value;
 
     }
 };
+
+//Open all the way
 COUCH.setPosition(100);
 setTimeout(() => {
-COUCH.setPosition(1);
+    // after 10 sec, close it
+    COUCH.setPosition(1);
 }, 10000);
